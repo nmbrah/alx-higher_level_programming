5c7bac8c94dc93342831796424189dba59ab3126
nmbrah <nanambrah22@gmail.com>
2023-03-08 12:18:07 -0800

    m

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(0, 99):
+    print('{} = 0x{:x}'.format(num, num))
+#!/usr/bin/python3
+for num in range(0, 99):
+    print('{} = 0x{:x}'.format(num, num))
+#!/usr/bin/python3
+for num in range(0, 99):
+    print('{} = 0x{:x}'.format(num, num))
